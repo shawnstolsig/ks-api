b6ae3fbb83fad3d2c8e4102de5cb426313959de3
Shawn Stolsig <shawnstolsig@gmail.com>
2021-01-30 21:07:42 -0800

    added reset route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const { sequelize, Sequelize } = require('../models/index')
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+router.get('/reset', async (req, res, next) => {
+  const { Battle, ClanResult, Clan, PlayerResult, Player, Stage } = sequelize.models
+  await Battle.destroy({
+    where: {}
+  })
+  await ClanResult.destroy({
+    where: {}
+  })
+  await Clan.destroy({
+    where: {}
+  })
+  await PlayerResult.destroy({
+    where: {}
+  })
+  await Player.destroy({
+    where: {}
+  })
+  await Stage.destroy({
+    where: {}
+  })
+
+
+  res.json( { message: 'reset success' });
 });
 
 module.exports = router;
